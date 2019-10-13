211a0eaffe9accd60c1866e8c7b73b1504c8b01b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-10-13 23:52:15 +0700

    chore: rebuild assets
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const { default: ImageminPlugin } = require('imagemin-webpack-plugin')
+
 module.exports = {
   baseUrl: process.env.BASE_URL,
   css: {
@@ -15,7 +17,19 @@ module.exports = {
       exclude: ['CNAME']
     }
   },
+  devServer: {
+    historyApiFallback: {
+      rewrites: [
+        { from: /./, to: '/public/404.html' }
+      ]
+    }
+  },
   chainWebpack: config => {
-    // .
+    config.plugin('imagemin').use(ImageminPlugin, [
+      {
+        disable: process.env.NODE_ENV !== 'production',
+        test: /\.(jpe?g|png|gif|svg)$/i
+      }
+    ])
   }
 }
