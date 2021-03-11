960abe805770c424315038bfb07182d67e5ac112
feryardiant <ferywardiyanto@gmail.com>
2021-03-11 21:07:37 +0000

    build: generate github pages

--- assets/[...all].b16e6dff.js
+++
@@ -1,1 +0,0 @@
-import{a as n,c as o,d as r}from"./vendor.536cbcb3.js";const a={},t={class:"container py-5"},c=r("h1",null,"404 Not Found",-1);a.render=function(r,a){return n(),o("main",t,[c])};export default a;
---
+++ assets/[...all].c642fceb.js
@@ -0,0 +1,1 @@
+import{a as n,c as a,d as o}from"./vendor.4dbaf7be.js";const r={},e={class:"container py-5"},t=o("h1",null,"404 Not Found",-1);r.render=function(o,r){return n(),a("main",e,[t])};export default r;
