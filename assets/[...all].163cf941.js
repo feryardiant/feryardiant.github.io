9c353d2ce6e335d54f31358728752caf5c9656d4
feryardiant <ferywardiyanto@gmail.com>
2021-03-27 16:37:59 +0000

    build: generate github pages

---
+++ assets/[...all].163cf941.js
@@ -0,0 +1,1 @@
+import{a as n,c as e,e as o}from"./vendor.ee866f1b.js";const r={},a={class:"container py-5"},t=o("h1",null,"404 Not Found",-1);r.render=function(o,r){return n(),e("main",a,[t])};export default r;
