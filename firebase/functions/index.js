6e840cae8a36b72d6097b63deaa7ba0ffa99433b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2021-03-29 07:17:56 +0700

    fix(firebase): use mailparser lib to handle inbound mail
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,105 +1,98 @@
-const { https, config, firestore } = require('firebase-functions')
+const { https, config, firestore, logger } = require('firebase-functions')
 const admin = require('firebase-admin')
-admin.initializeApp()
 
-const { parseMail } = require('./lib')
-const db = admin.firestore()
-const conf = config()
-
-const sg = require('@sendgrid/mail')
-
-const contacts = db.collection('contacts')
-const messages = db.collection('messages')
-
-exports.autoReply = firestore.document('messages/{subject}/contents/{content}').onCreate(async (snap, { params }) => {
-  const replyEmail = conf.app.mail
-  const headers = {
-    'In-Reply-To': params.content
-  }
-
-  const messageRef = messages.doc(params.subject)
-  const message = await messageRef.get()
-  const content = snap.data()
-  const mail = message.data()
-  const mails = []
-
-  if (!mail.replied) {
-    mails.push({
-      to: mail.from,
-      from: mail.to[0],
-      replyTo: mail.to[0],
-      headers: headers,
-      subject: `Re: ${params.subject}`,
-      text: 'Hi there,\n\nThank you for reaching me out and this is auto-reply email, will be back to you ASAP.\n\nBest regards\nFery W.'
-    })
-  }
-
-  const toMe = {
-    to: conf.app.author.email,
-    from: {
-      name: mail.from.name,
-      email: mail.to[0].email
-    },
-    replyTo: mail.from,
-    headers: headers,
-    subject: params.subject,
-    text: content['text/plain']
-  }
-
-  if (content['text/html'].length) {
-    toMe.html = content['text/html']
-  }
-
-  mails.push(toMe)
-  console.log(mails)
-
-  try {
-    sg.setApiKey(conf.sendgrid.key)
-    await sg.send(mails)
-
-    if (!mail.replied) {
-      await messageRef.update({
-        replied: true
-      })
-    }
-  } catch (err) {
-    console.error(err.toString())
-  }
+admin.initializeApp({
+  storageBucket: 'feryardiant-id.appspot.com'
 })
 
-exports.mail = https.onRequest(async (req, res) => {
-  if (req.method.toLowerCase() !== 'post') {
-    return res.status(403).send('Error')
+const express = require('express')
+const app = express()
+
+const db = admin.firestore()
+const bucket = admin.storage().bucket()
+const conf = config()
+
+const parseInboundMail = require('./mail')
+
+const attachments = db.collection('attachments')
+const conversations = db.collection('conversations')
+const people = db.collection('people')
+
+exports.mail = https.onRequest(parseInboundMail(app, db, logger, bucket))
+
+exports.autoReply = firestore.document('messages/{messageId}').onCreate(async (snap, { params }) => {
+  const envelope = snap.data()
+  const conversation = conversations.doc(envelope.topic || envelope.subject)
+  const messages = conversation.collection('messages')
+  const participants = conversation.collection('participants')
+
+  const batch = db.batch()
+  await people.doc(envelope.from.address).set(envelope.from)
+  await messages.doc(params.messageId).set(envelope)
+  await participants.doc(envelope.from.address).set(envelope.from)
+
+  for (let party of ['to', 'cc']) {
+    if (!envelope[party]) continue
+
+    for (let participant of envelope[party]) {
+      await participants.doc(participant.address).set(participant)
+    }
   }
 
-  const mail = parseMail(req)
+  const messageAttachments = snap.get('attachments')
 
-  try {
-    await db.runTransaction(async trans => {
-      const sender = contacts.doc(mail.from.email)
-      await sender.set(mail.from)
-
-      const message = messages.doc(mail.subject)
-      const email = await trans.get(message)
-
-      if (!email.exists) {
-        await trans.set(message, {
-          from: mail.from,
-          to: mail.to,
-          replied: false
-        })
-      }
-
-      const envelope = {}
-      for (const content of mail.contents) {
-        envelope[content.content_type.split(';')[0]] = content.decoded || ''
-      }
-
-      await trans.set(message.collection('contents').doc(mail.message_id), envelope)
-    })
-  } catch (err) {
-    console.error('Transaction error:', err)
+  if (messageAttachments) {
+    for (let attachment of messageAttachments) {
+      attachments.doc(attachment.cid).set(attachment)
+    }
   }
 
-  return res.send('Ok')
+  await batch.commit()
+
+  // const message = await conversation.get()
+  // const mail = message.data()
+  // const mails = []
+
+  // if (!mail.replied) {
+  //   mails.push({
+  //     to: mail.from,
+  //     from: mail.to[0],
+  //     replyTo: mail.to[0],
+  //     headers: headers,
+  //     subject: `Re: ${params.subject}`,
+  //     text: 'Hi there,\n\nThank you for reaching me out and this is auto-reply email, will be back to you ASAP.\n\nBest regards\nFery W.'
+  //   })
+  // }
+
+  // const toMe = {
+  //   to: conf.app.author.email,
+  //   from: {
+  //     name: mail.from.name,
+  //     email: mail.to[0].email
+  //   },
+  //   replyTo: mail.from,
+  //   headers,
+  //   subject: params.subject,
+  //   text: content['text/plain']
+  // }
+
+  // if (content['text/html']?.length) {
+  //   toMe.html = content['text/html']
+  // }
+
+  // mails.push(toMe)
+  // logger.log(mails)
+
+  // try {
+  //   sg.setApiKey(conf.sendgrid.key)
+  //   await sg.send(mails)
+
+  //   if (!mail.replied) {
+  //     await messageRef.update({
+  //       replied: true
+  //     })
+  //   }
+  // } catch (err) {
+  //   logger.error(err)
+  // }
 })
