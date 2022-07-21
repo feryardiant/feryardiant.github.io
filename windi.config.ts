aa20c9f48ecfe1c8301eb800b58bcf1e2b75a386
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-07-22 06:15:14 +0700

    chore(dev): init typescript
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ windi.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'windicss/helpers'
+import defaultTheme from 'windicss/defaultTheme'
+
+export default defineConfig({
+  darkMode: 'class',
+  extract: {
+    include: ['index.html', 'src/**/*.{vue,html,js,ts}'],
+    exclude: ['node_modules', '.git'],
+  },
+  plugins: [
+    require('windicss/plugin/typography'),
+    require('windicss/plugin/forms'),
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: false,
+            h1: {
+              fontWeight: '700',
+              marginTop: false,
+              marginBottom: false,
+              margin: '0',
+            },
+            figure: {
+              marginTop: false,
+            },
+            img: {
+              borderRadius: '.25rem',
+              border: '1px solid #D1D5DB',
+            },
+          },
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['Merriweather Sans', ...defaultTheme.fontFamily.sans],
+      mono: ['Fira Code', defaultTheme.fontFamily.mono],
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+      css: {
+        padding: '1rem',
+      },
+    },
+  },
+})
