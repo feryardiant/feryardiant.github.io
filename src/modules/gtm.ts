32e83f7cfccf504284dfcca34f2b32448f468881
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-11-15 10:57:27 +0700

    build: fix gtm config
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/modules/gtm.ts
+++ src/modules/gtm.ts
@@ -2,11 +2,11 @@ import { createGtm, useGtm } from '@gtm-support/vue-gtm'
 import type { UserModule } from '~/types'
 
 export const install: UserModule = ({ app, isClient, router, routes }) => {
-  if (!isClient && !import.meta.env.VITE_GTM_KEY)
+  if (!isClient && !import.meta.env.VITE_GTM_ID)
     return
 
   const gtm = createGtm({
-    id: import.meta.env.VITE_GTM_KEY,
+    id: import.meta.env.VITE_GTM_ID,
     vueRouter: router,
     debug: import.meta.env.DEV,
   })
