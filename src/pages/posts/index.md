c8f8d60cae2e6c158acaa6a06411e104de8ca804
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-11-15 10:56:30 +0700

    fix: fix blog page issues
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/pages/posts/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Blog
-menu:
-  position: top
-  title: Blog
----
-<posts-list />
---
+++ src/pages/posts/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <posts-list />
+</template>
+
+<route lang="yaml">
+title: Blog
+menu:
+  position: top
+  title: Blog
+</route>
